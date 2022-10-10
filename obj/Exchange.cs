99d84d3c6f708e2c327be3c85b671d6d634c5fe2
Madman283 <93745722+Madman283@users.noreply.github.com>
2022-10-10 14:04:03 -0500

    Coffee

---
+++ obj/Exchange.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static EventDemo.Utility;
+
+namespace EventDemo
+{
+    class Exchange
+    {
+        private string name = "Your current beverage temp sim";
+        List<Stock> Stocks = new List<Stock>()
+        {
+            new Stock("Coffee"),
+            new Stock("Hot Chocolate"),
+            new Stock("Tea")
+        };
+        public void Open()
+        {
+            SetStockPrices();
+            PrintAllStockPrices();
+            Run();
+        }
+        private void Menu()
+        {
+            Print("Press x to exit, any other key to check for temps.");
+            string input = Console.ReadLine();
+            if (input.ToLower() != "x")
+            {
+                Run();
+            }
+
+        }
+        private void Run()
+        {
+            ClearScreen();
+            Print(name);
+            Print("--------------------------");
+            PrintAllStockPrices();
+            Print("--------------------------");
+        
+            
+            //call setstockprices on all stocks in the list
+            foreach (Stock s in Stocks)
+            {
+                s.PriceFluctuation();
+            }
+            Menu();
+
+        }
+        private void SetStockPrices()
+        {
+            foreach (Stock s in Stocks)
+            {
+                // Register with the PriceChanged event
+                s.PriceChanged += s.stock_PriceChanged;
+                s.Price = Utility.RandomNumber.Next(71, 85);
+            }
+        }
+      
+        private void PrintAllStockPrices()
+        {
+            foreach (Stock s in Stocks)
+            {
+                Print($"Your drink of {s.Symbol} is currently at {s.Price} degrees celcius");
+            }
+        }
+
+    }
+}
---
+++ obj/Stock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static EventDemo.Utility;
+
+namespace EventDemo
+{
+    class Stock
+    {
+        string symbol;
+        decimal price;
+
+        public Stock(string symbol) { this.symbol = symbol; }
+
+        public event EventHandler<PriceChangedEventArgs> PriceChanged;
+
+        protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+        {
+            PriceChanged?.Invoke(this, e);
+        }
+
+        public void PriceFluctuation()
+        {
+            if (GetRandomInteger(2)==1)
+            Price = Price + GetRandomInteger(-10, 0);
+            if (Price <= 5)
+            {
+                Print($"You waited to long to drink {symbol}! Now it is cold.");
+            }
+            
+        }
+
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                OnPriceChanged(new PriceChangedEventArgs(oldPrice, price));
+            }
+        }
+
+        public string Symbol { get => symbol; set => symbol = value; }
+
+        public void stock_PriceChanged(object sender, PriceChangedEventArgs e)
+        {
+            if (e.LastPrice != e.NewPrice)
+                Print($"{Symbol} temp has changed. Previous temp was {e.LastPrice}, new temp is {e.NewPrice}. The difference is {e.LastPrice - e.NewPrice} degrees celcius.");
+        }
+
+        public class PriceChangedEventArgs : EventArgs
+        {
+            public readonly decimal LastPrice;
+            public readonly decimal NewPrice;
+
+            public PriceChangedEventArgs(decimal lastPrice, decimal newPrice)
+            {
+                LastPrice = lastPrice; NewPrice = newPrice;
+            }
+        }
+
+    }
+}
